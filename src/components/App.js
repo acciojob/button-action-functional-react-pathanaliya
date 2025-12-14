40c5a5af0bcf9c35100948e2e022041726876eb7
pathanaliya <aliyapathan2019@gmail.com>
2025-12-14 18:58:42 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  // state to control paragraph visibility
+  const [showPara, setShowPara] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* Button with required ID */}
+      <button id="click" onClick={() => setShowPara(true)}>
+        Click Me
+      </button>
+
+      {/* Conditional rendering of paragraph */}
+      {showPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
     </div>
   );
 }
